1229872ff77a08364855dcd99b390398865a8195
manucarenjot <carenjotje@gmail.com>
2022-04-08 10:03:29 +0200

    Création de fichier

---
+++ Controller/AbstractController.php
@@ -0,0 +1,20 @@
+<?php
+
+abstract class AbstractController
+{
+    abstract public function index();
+
+    /**
+     * @param string $template
+     * @param array $data
+     * @return void
+     */
+    public function render(string $template, array $data = [])
+    {
+        ob_start();
+        require __DIR__ . '/../View/' . $template . '.html.php';
+        $html = ob_get_clean();
+        require __DIR__ . '/../View/base.html.php';
+        exit;
+    }
+}
---
+++ View/base.html.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<header>
+
+</header>
+
+<main class="container">
+    <?= $html ?>
+</main>
+
+<footer>
+
+</footer>
+<script src=""></script>
+</body>
+</html>
